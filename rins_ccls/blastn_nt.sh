e7f71c8401eafbb16ce4ca5a6c0ef65b94e4c50e
Jake <github@jakewendt.com>
2013-05-29 15:27:25 -0700

    Deving cluster blast script

--- rins_ccls/blastn_nt.sh
+++ rins_ccls/blastn_nt.sh
@@ -1,18 +1,24 @@
 #!/bin/sh
 
-db='/Volumes/cube/working/indexes/nt'
-#	-db=/my/home/jwendt/blast/nt \
-
-#	gonna try to use this on the Epi Cluster
+if [ `uname -n` = "fxdgroup-169-229-196-225.sph.berkeley.edu" ] ; then
+	#	echo "on dev"
+	db='/Volumes/cube/working/indexes/nt'
+	cmdbase=''
+else
+	db='/my/home/jwendt/blast/nt'
+	cmdbase='srun --exclusive -n 1'
+fi
 
 while [ $# -ne 0 ] ; do
-	echo $1
 
 	if [ -f $1 ] ; then
-		echo "blastn -query=$1 -db=$db -evalue 0.05 -outfmt 0 > $1.blastn.txt"
+		cmd="$cmdbase blastn -query $1 -db $db -evalue 0.05 -outfmt 0 -out $1.blastn.txt &"
+		touch $1.blastn.txt.start_touch
+		eval $cmd	#	need to eval to use the &
 	else
 		echo "$1 doesn't seem to be a file."
 	fi
 
 	shift
 done #	while [ $# -ne 0 ] ; do
+
