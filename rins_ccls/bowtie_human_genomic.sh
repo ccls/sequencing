ab658fe1df86a515366c39c6f09c272658ed1347
Jake <github@jakewendt.com>
2013-06-01 10:39:12 -0700

    Deving temp script.

--- rins_ccls/bowtie_human_genomic.sh
+++ rins_ccls/bowtie_human_genomic.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-
+{
 #	quick script to test the removal by human_genomic indexes
 
 ifile=raw_not_hg18_hg19_Blast1_Blast2_Homo_nt_human_1_2
@@ -12,3 +12,4 @@ for n in 01 02 03 04 05 06 07 08 09 10 11 12 13 ; do
 	ifile=$ofile
 done
 
+} 1>>bowtie_human_genomic.out 2>&1
