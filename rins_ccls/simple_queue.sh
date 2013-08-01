b10df1a11f259b0498fb0e97895802b364585ac6
Jake <github@jakewendt.com>
2013-08-01 08:43:49 -0700

    Changed simple queue timeout from 1000 to 5000.

--- rins_ccls/simple_queue.sh
+++ rins_ccls/simple_queue.sh
@@ -12,7 +12,7 @@ database_file_name="$HOME/simple_queue.db"
 #	It seems that attempting to read and write to a table when another script is 
 #	attempting the same can result in many collisions which result in silent failure.
 #	Not entirely silent, but nevertheless.  This is due to the table being locked.
-#	Changing the default timeout of 0 to 1000 ms or something will allow the command
+#	Changing the default timeout of 0 to 5000 ms or something will allow the command
 #	to wait for the lock to be removed.  This could be done several ways, but must
 #	be done again and again with each call.  Apparently it can't be set in the database
 #	itself.  It also can't be in the sql command passed to sqlite.  It could either
@@ -21,14 +21,14 @@ database_file_name="$HOME/simple_queue.db"
 #
 #	-init mysqliteinitfile
 #
-#	-cmd '.timeout 1000' 
+#	-cmd '.timeout 5000' 
 #
 #	As this should be used in virtually all calls, or at least doesn't hurt
 #	those that don't really need it, I'm gonna set a variable here for this.
 #
-#sqlite="sqlite3 -cmd '.timeout 1000' $database_file_name "
+#sqlite="sqlite3 -cmd '.timeout 5000' $database_file_name "
 #	using this variable doesn't work?
-#sqlite3 -cmd '.timeout 1000' /Users/jakewendt/simple_queue.db
+#sqlite3 -cmd '.timeout 5000' /Users/jakewendt/simple_queue.db
 #sqlite3: Error: too many options: "select * from queue"
 #Use -help for a list of options.
 #sqlite3: Error: too many options: "create table queue(id integer primary key autoincrement, command text)"
@@ -36,7 +36,7 @@ database_file_name="$HOME/simple_queue.db"
 #sqlite3: Error: too many options: "select * from queue"
 #Use -help for a list of options.
 #
-#sh-3.2$ sqlite="sqlite3 -cmd '.timeout 1000' $HOME/simple_queue.db "
+#sh-3.2$ sqlite="sqlite3 -cmd '.timeout 5000' $HOME/simple_queue.db "
 #sh-3.2$ $sqlite
 #Error: unrecognized token: "'.timeout"
 #Error: near "/": syntax error
@@ -47,7 +47,7 @@ database_file_name="$HOME/simple_queue.db"
 
 
 pop(){
-	r=`sqlite3 -cmd '.timeout 1000' -line $database_file_name "select * from queue order by id asc limit 1"`
+	r=`sqlite3 -cmd '.timeout 5000' -line $database_file_name "select * from queue order by id asc limit 1"`
 #	r=`$sqlite -line "select * from queue order by id asc limit 1"`
 	#	$r will NOT have the newlines
 	# id = 1 command = srun
@@ -60,14 +60,14 @@ pop(){
 	if [ "x$id" != "x"  ] ; then
 		command=`echo "$r" | grep "^\s*command = " | awk -F= '{print $NF}'`
 		echo $command
-		sqlite3 -cmd '.timeout 1000' $database_file_name "delete from queue where id = $id"
+		sqlite3 -cmd '.timeout 5000' $database_file_name "delete from queue where id = $id"
 #		$sqlite "delete from queue where id = $id"
 	fi
 }
 
 
 if [ ! -f $database_file_name ] ; then
-	sqlite3 -cmd '.timeout 1000' $database_file_name 'create table queue(id integer primary key autoincrement, command text)'
+	sqlite3 -cmd '.timeout 5000' $database_file_name 'create table queue(id integer primary key autoincrement, command text)'
 #	$sqlite 'create table queue(id integer primary key autoincrement, command text)'
 #else
 	#	echo 'found it'
@@ -83,13 +83,13 @@ case "$1" in
 		shift; pop;;
 	push )
 #		shift; $sqlite "insert into queue(command) values('$*')";;
-		shift; sqlite3 -cmd '.timeout 1000' $database_file_name "insert into queue(command) values('$*')";;
+		shift; sqlite3 -cmd '.timeout 5000' $database_file_name "insert into queue(command) values('$*')";;
 	size | count | length )
 #		$sqlite "select count(*) from queue" ;;
-		sqlite3 -cmd '.timeout 1000' $database_file_name "select count(*) from queue" ;;
+		sqlite3 -cmd '.timeout 5000' $database_file_name "select count(*) from queue" ;;
 	* )
 #		$sqlite "select * from queue"
-		sqlite3 -cmd '.timeout 1000' $database_file_name "select * from queue"
+		sqlite3 -cmd '.timeout 5000' $database_file_name "select * from queue"
 esac
 
 
