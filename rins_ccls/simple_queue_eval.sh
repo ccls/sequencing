66529df5eb201741c99c2258c67d3f6e513fdc88
Jake <github@jakewendt.com>
2014-12-15 12:56:21 -0800

    Mods to add cluster head node name to pid and log files due to creation of new cluster.

--- rins_ccls/simple_queue_eval.sh
+++ rins_ccls/simple_queue_eval.sh
@@ -12,8 +12,12 @@
 : ${SLURM_JOBID:="000000"}
 : ${SLURM_TASK_PID:="0000"}
 
-touch ~/ec/pids/$SLURMD_NODENAME.$SLURM_JOBID.$SLURM_TASK_PID
+cluster=`uname -n`
+pid_file="~/ec/pids/$cluster.$SLURMD_NODENAME.$SLURM_JOBID.$SLURM_TASK_PID"
+log_file="~/ec/logs/$cluster.$SLURMD_NODENAME.$SLURM_JOBID.$SLURM_TASK_PID"	#	NO .log
+touch $pid_file
 
+#	examples:
 #	SLURM_NODELIST=n0006
 #	SLURM_JOBID=231351
 #	SLURM_JOB_NAME=slurm_env.sh
@@ -24,7 +28,8 @@ touch ~/ec/pids/$SLURMD_NODENAME.$SLURM_JOBID.$SLURM_TASK_PID
 	echo "Starting at ..."
 	date
 
-	while [ -f ~/ec/pids/$SLURMD_NODENAME.$SLURM_JOBID.$SLURM_TASK_PID ]; do
+#	while [ -f ~/ec/pids/$SLURMD_NODENAME.$SLURM_JOBID.$SLURM_TASK_PID ]; do
+	while [ -f $pid_file ]; do
 
 		#
 		#	FYI. Counting can kill as it isn't dealt with if sqlite has a hissy fit.
@@ -40,28 +45,26 @@ touch ~/ec/pids/$SLURMD_NODENAME.$SLURM_JOBID.$SLURM_TASK_PID
 			echo "- - - - - - - - - -"
 			echo "Popped ..."
 			echo $cmd
-			#
-			#	remove everything before blastn
-			#		srun --share --job-name=003042_viral_genomic 
-			#
-			#	AND remove the trailing &!!!!!
-			#
-			#	Queue should no longer contain these.
-#			cmd=`echo $cmd | sed 's/^srun.*blastn -q/blastn -q/' | sed 's/ &$//'`
+			
 			echo "Executing ..."
 			echo $cmd
 			date
 			output=`$cmd`
 			status=$?
+			#
+			#	Sadly, blast failure does not always return a failure code.
+			#
 			if [ $status -ne 0 ]; then
-				echo $cmd >> ~/ec/logs/$SLURMD_NODENAME.$SLURM_JOBID.$SLURM_TASK_PID.FAILED
+#				echo $cmd >> ~/ec/logs/$SLURMD_NODENAME.$SLURM_JOBID.$SLURM_TASK_PID.FAILED
+				echo $cmd >> $log_file.FAILED
 			fi
-			echo "Completed. :${output}:"	#	"output" will always be blank
+			echo "Completed. :${output}:"	#	"output" will always be blank ... making this kinda pointless
 			date
 			echo "- - - - - - - - - -"
 		else
 			echo "Available :${available}: not greater than zero.  Status:${available_status}:. Commiting suicide.  Goodbye cruel world!"
-			rm ~/ec/pids/$SLURMD_NODENAME.$SLURM_JOBID.$SLURM_TASK_PID
+#			rm ~/ec/pids/$SLURMD_NODENAME.$SLURM_JOBID.$SLURM_TASK_PID
+			rm $pid_file
 		fi
 
 	done
@@ -70,12 +73,16 @@ touch ~/ec/pids/$SLURMD_NODENAME.$SLURM_JOBID.$SLURM_TASK_PID
 	echo "Ending at ..."
 	date
 
-} 1>>~/ec/logs/$SLURMD_NODENAME.$SLURM_JOBID.$SLURM_TASK_PID.log 2>&1
+#} 1>>~/ec/logs/$SLURMD_NODENAME.$SLURM_JOBID.$SLURM_TASK_PID.log 2>&1
+} 1>> $log_file.log 2>&1
 
 exit;
 
 #	Run this 4 times.  14 seems to be the max callable and 4*14=56.  Conveniently.
 #	srun --share --partition=ccls --ntasks=14 simple_queue_eval.sh &
+#	Actually, don't do the above.  The ntasks doesn't let go.  If some fail, 
+#	the process is still running according to srun and you can't start 
+#	another one, even individually.  
 
 #	There are actually 56 run units available on ccls (not 52)
 #	n0003 - 16
@@ -83,3 +90,5 @@ exit;
 #	n0005 - 16
 #	n0006 - 4
 #	n0007 - 4
+#	6 and 7 are now gone.
+
