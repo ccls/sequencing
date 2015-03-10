7422bcf4b23ce0d61230554183768a6554cad162
Jake <github@jakewendt.com>
2015-03-10 14:52:10 -0700

    Cleanup

--- rins_ccls/srun_ec_quick_dark.sh
+++ rins_ccls/srun_ec_quick_dark.sh
@@ -7,17 +7,8 @@ fi
 
 base=`basename $PWD`
 
-#while [ $# -ne 0 ] ; do
-#	echo $1
-#	base=${1%.*}		#	drop the .fasta extension
-#	name=${base#*/}	#	just in case given path
-
-#		--begin=23:00 \
-#		--partition=bigmem \
-#		--exclude=n[0000-0029] \
-
 srun --nice --share --partition=bigmem \
-	--job-name="quick_dark${base}" \
+	--job-name="dark_${base}" \
 	--cpus-per-task=8 \
 	--error=$base.quick_dark.errors.`date "+%Y%m%d%H%M%S"` \
 	--output=$base.quick_dark.output.`date "+%Y%m%d%H%M%S"` \
