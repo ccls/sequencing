7d983c0dbf67e371e6cd53f3ec8f66bb3d001538
Jake <github@jakewendt.com>
2015-03-16 16:05:55 -0700

    Added a couple more srun_ scripts.

---
+++ rins_ccls/srun_bam2fastx_fastq.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+if [ $# -eq 0 ]; then
+	echo "I need at least one filename"
+	exit
+fi
+
+while [ $# -ne 0 ] ; do
+	echo $1
+	base=${1%.*}		#	drop the .bam extension
+	name=${base#*/}	#	just in case given path
+
+#		--begin=23:00 \
+#		--partition=bigmem \
+#		--exclude=n[0000-0029] \
+
+	srun --nice --share \
+		--exclude=n[0000-0009] \
+		--job-name="bam2fastx_${name}" \
+		--cpus-per-task=8 \
+		--error=$base.bam2fastx.errors.`date "+%Y%m%d%H%M%S"` \
+		--output=$base.bam2fastx.output.`date "+%Y%m%d%H%M%S"` \
+		bam2fastx --fastq --all -N \
+			-o $name.fastq \
+			$1 &
+
+	shift
+done
---
+++ rins_ccls/srun_lane_fastq.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+if [ $# -eq 0 ]; then
+	echo "I need at least one filename"
+	exit
+fi
+
+while [ $# -ne 0 ] ; do
+	echo $1
+	base=${1%.*}		#	drop the .bam extension
+	name=${base#*/}	#	just in case given path
+
+#		--begin=23:00 \
+#		--partition=bigmem \
+#		--exclude=n[0000-0029] \
+
+	srun --nice --share \
+		--exclude=n[0000-0009] \
+		--job-name="lane_fastq_${name}" \
+		--cpus-per-task=8 \
+		--error=$base.lane_fastq.errors.`date "+%Y%m%d%H%M%S"` \
+		--output=$base.lane_fastq.output.`date "+%Y%m%d%H%M%S"` \
+		awk '
+			( /\/1$/ ){ f=sprintf("%s.1.fastq",substr(FILENAME,0,index(FILENAME,".fastq")-1)) }
+			( /\/2$/ ){ f=sprintf("%s.2.fastq",substr(FILENAME,0,index(FILENAME,".fastq")-1)) }
+			{ print >> f }
+		' $1 &
+
+	shift
+done
