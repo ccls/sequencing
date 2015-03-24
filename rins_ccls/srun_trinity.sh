ea990b1789eed316ad9c18ee91196331695e6ba4
Jake <github@jakewendt.com>
2015-03-24 10:24:57 -0700

    Added file extension checker.

--- rins_ccls/srun_trinity.sh
+++ rins_ccls/srun_trinity.sh
@@ -10,6 +10,14 @@ while [ $# -ne 0 ] ; do
 	base=${1%.*}		#	drop the .fasta extension
 	name=${base#*/}	#	just in case given path
 
+#	grab first character of file 
+#		@ = fastq
+#		> = fasta
+#	filetype=`head -c 1 $1`
+
+	#	This better be a or q
+	filetype=${1:(-1)}
+
 #		--begin=23:00 \
 #		--partition=bigmem \
 #		--exclude=n[0000-0029] \
@@ -21,14 +29,14 @@ while [ $# -ne 0 ] ; do
 		--output=$base.trinity.output.`date "+%Y%m%d%H%M%S"`.nobackup \
 		Trinity --CPU 8 \
 			--bypass_java_version_check \
-			--normalize_reads \
-			--seqType fa \
+			--seqType f${filetype} \
 			--run_as_paired \
 			--min_contig_length 100 \
 			--max_memory 40G \
 			--single $1 \
 			--output $base.trinity_output.nobackup &
 
+	#		--normalize_reads \
 	#		--full_cleanup \
 	#	/my/home/ccls/data/working/TCGA_Glioma/trinity_wrapper.sh $1 &
 	#mv $PWD/$base.trinity_output.nobackup.Trinity.fasta $PWD/$base.trinity.fasta
