373641113283863f39a9dfd5e01f37870f676bb4
Jake <github@jakewendt.com>
2015-05-06 09:02:45 -0700

    Fine tuning of the sam_to_bam script.

--- rins_ccls/sam_to_bam.sh
+++ rins_ccls/sam_to_bam.sh
@@ -5,15 +5,23 @@ while [ $# -ne 0 ] ; do
 		echo "Creating bam from (assumed) sam file $1"
 
 		base=${1%.*}		#	drop the extension
+		chmod -w $1
 
 		echo "Converting to bam"
+		#	Usage:   samtools view [options] <in.bam>|<in.sam>|<in.cram> [region ...]
 		samtools view -S -b -o $base.unsorted.bam $1
+		chmod -w $base.unsorted.bam
 
 		echo "Sorting"
-		samtools sort $base.unsorted.bam $base.bam
+		#	Usage: samtools sort [options...] [in.bam]
+		#	Legacy usage: samtools sort [options...] <in.bam> <out.prefix>
+		samtools sort $base.unsorted.bam $base
+		chmod -w $base.bam
 
 		echo "Indexing"
+		#	Usage: samtools index [-bc] [-m INT] <in.bam> [out.index]
 		samtools index $base.bam
+		chmod -w $base.bam.bai
 	else
 		echo "$1 not a file? Skipping"
 	fi
