e23c36c9fc65219fd70e02f024b6e7c20de42048
Jake <github@jakewendt.com>
2015-05-13 10:14:41 -0700

    Change job and output file names.

---
+++ rins_ccls/srun_samtools_sam_to_bam.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+function usage(){
+	echo
+	echo "Usage:"
+	echo
+	echo "`basename $0` samfile(s)"
+	echo
+	echo "Example:"
+	echo "  `basename $0` /my/path/*sam"
+	echo
+	exit 1
+}
+#	Basically, this is TRUE AND DO ...
+[ $# -eq 0 ] && usage
+
+
+while [ $# -ne 0 ] ; do
+	echo $1
+	base=${1%.*}		#	drop the .bam extension
+	name=${base#*/}	#	just in case given path
+
+#		--begin=23:00 \
+#		--partition=bigmem \
+#		--exclude=n[0000-0029] \
+
+	srun --nice --share \
+		--partition=bigmem \
+		--exclude=n[0000-0009] \
+		--job-name="samtools_sam_to_bam_${name}" \
+		--cpus-per-task=8 \
+		--error=$base.samtools_sam_to_bam_.errors.`date "+%Y%m%d%H%M%S"`.nobackup \
+		--output=$base.samtools_sam_to_bam_.output.`date "+%Y%m%d%H%M%S"`.nobackup \
+		sam_to_bam.sh $1 &
+
+	shift
+done
