d62a789086a3f5bea238ff4ca403819d3667fbea
Jake <github@jakewendt.com>
2015-06-01 14:58:13 -0700

    Created new fasta_read_length_filter script.

---
+++ rins_ccls/fasta_read_length_filter.sh
@@ -0,0 +1,63 @@
+#!/usr/bin/env bash
+
+function usage(){
+	echo
+	echo "Usage:"
+	echo
+	echo "`basename $0` [--length INTEGER] [--side l/r] <fasta files(s)>"
+	echo
+	echo "Example:"
+	echo "  `basename $0` -s r -l 30 /my/path/*fasta"
+	echo
+	echo "Defaults:"
+	echo "  --length/-l ..... : 50"
+	echo "  --side/-s   ..... : l"
+	echo
+	exit 1
+}
+#	Basically, this is TRUE AND DO ...
+[ $# -eq 0 ] && usage
+
+length=50
+side='l'
+
+while [ $# -ne 0 ] ; do
+	case $1 in
+		-l|--l*)
+			shift; length=$1; shift ;;
+		-s|--s*)
+			shift; side=$1; shift ;;
+		-*)
+			echo ; echo "Unexpected args from: ${*}"; usage ;;
+		*) 
+			break;;
+	esac
+done
+
+while [ $# -ne 0 ] ; do
+	echo $1
+#	base=${1%.*}		#	drop the extension
+#	ext=${1##*.}		#	grab the extension
+#	name=${base#*/}	#	just in case given path, drop the path
+
+	awk -v l=$length -v s=$side 'BEGIN{ n=r="" }
+		END{ o() }
+		function o(){
+			if( length(r) >= l ){
+				print n
+				if( s == "l") {
+					print substr(r,1,l)
+				} else {
+					print substr(r,length(r)-l+1,l)
+				}
+			}
+		}
+		( /^>/ ){
+			o()
+			n=$0
+			r=""
+		}
+		( !/^>/ ){ r=r $0 }' $1
+
+	shift
+done
