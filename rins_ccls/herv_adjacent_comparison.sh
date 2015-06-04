3344028b99b411ffb83f2c8e658d21d22c5f59d1
Jake <github@jakewendt.com>
2015-06-04 14:59:10 -0700

    Doing comparison in both directions.

--- rins_ccls/herv_adjacent_comparison.sh
+++ rins_ccls/herv_adjacent_comparison.sh
@@ -16,33 +16,45 @@ function usage(){
 
 
 echo $1, $2
-base=${1%.*}		#	drop the extension
-ext=${1##*.}		#	grab the extension
-#	name=${base#*/}	#	just in case given path, drop the path
+other=$2
+while [ $# -ne 0 ] ; do
 
-cmd="fastx_collapser -i $1 -o $base.collapsed.$ext"
-echo $cmd
-$cmd
+	base=${1%.*}		#	drop the extension
+	ext=${1##*.}		#	grab the extension
+	#	name=${base#*/}	#	just in case given path, drop the path
 
-cmd="bowtie2-build $base.collapsed.$ext $base"
-echo $cmd
-$cmd
+	cmd="fastx_collapser -i $1 -o $base.collapsed.$ext"
+	echo $cmd
+	$cmd
 
-cmd="bowtie2 -f -x $base -U $2 -S $base.sam"
-echo $cmd
-$cmd
-\rm *bt2
+	cmd="bowtie2-build $base.collapsed.$ext $base"
+	echo $cmd
+	$cmd
 
-cmd="samtools view -S -h $base.sam"
-echo $cmd
-$cmd | awk -v out=$base.counts '
-	( /^@SQ/ ){ ref[substr($2,4)] = 0 }
-	( !/^@/ ){ ref[$3]++ }
-	END{
-		for ( key in ref ) {
-			print key, ref[key] >> out
-		}
-	}'
+	cmd="bowtie2 -f -x $base -U $other -S $base.sam"
+	echo $cmd
+	$cmd
+	\rm $base*bt2
 
-#	awk options must be in specific order 
-#	(--posix needed to be after the -v's but is no longer needed anyway)
+	#	-F 4 will stop the *'s from coming out at night.
+
+	cmd="samtools view -S -h -F 4 $base.sam"
+	echo $cmd
+	$cmd | awk -v out=$base.tmp.counts '
+		( /^@SQ/ ){ ref[substr($2,4)] = 0 }
+		( !/^@/ ){ ref[$3]++ }
+		END{
+			for ( key in ref ) {
+				print key, ref[key] >> out
+			}
+		}'
+
+	#	awk options must be in specific order
+	#	(--posix needed to be after the -v's but is no longer needed anyway)
+
+	cat $base.tmp.counts | tr " " "\t" | tr "-" "\t" | sort -k2,2n > $base.counts
+	\rm $base.tmp.counts
+
+	other=$1
+	shift
+done
