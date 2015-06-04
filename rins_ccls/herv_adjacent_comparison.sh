cdb3cfb5a11329952ee0d153a70eadbbe978f01c
Jake <github@jakewendt.com>
2015-06-04 10:12:24 -0700

    Created herv_adjacent_comparison.sh

---
+++ rins_ccls/herv_adjacent_comparison.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+
+function usage(){
+	echo
+	echo "Usage:"
+	echo
+	echo "`basename $0` tumor.fasta normal.fasta"
+	echo
+	echo "Example:"
+	echo "  `basename $0` tumor.fasta normal.fasta"
+	echo
+	exit 1
+}
+#	Basically, this is TRUE AND DO ...
+[ $# -ne 2 ] && usage
+
+
+echo $1, $2
+base=${1%.*}		#	drop the extension
+ext=${1##*.}		#	grab the extension
+#	name=${base#*/}	#	just in case given path, drop the path
+
+fastx_collapser -i $1 -o $base.collapsed.$ext
+
+bowtie2-build $base.collapsed.$ext $base
+
+bowtie2 -f -x $base -U $2 -S $base.sam
+\rm *bt2
+
+cmd="samtools view -S -h -F 4 $base.sam"
+echo $cmd
+$cmd | awk --posix '
+	( !/^@/ ){ ref[$3]++ }
+	END{
+		for ( key in ref ) {
+			print key, ref[key]
+		}
+	}'
+
