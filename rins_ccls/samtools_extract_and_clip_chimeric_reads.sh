cd891f265e70c07c59e67d5eb1a17afb78397f06
Jake <github@jakewendt.com>
2015-06-08 10:21:09 -0700

    Allow optional output of fastq.

--- rins_ccls/samtools_extract_and_clip_chimeric_reads.sh
+++ rins_ccls/samtools_extract_and_clip_chimeric_reads.sh
@@ -4,7 +4,7 @@ function usage(){
 	echo
 	echo "Usage:"
 	echo
-	echo "`basename $0` <samfile(s) or bamfile(s)>"
+	echo "`basename $0` [-q] <samfile(s) or bamfile(s)>"
 	echo
 	echo "Example:"
 	echo "  `basename $0` /my/path/*bam"
@@ -15,12 +15,35 @@ function usage(){
 	echo "          1 db entry is the 'beginning' of the start of the ltr."
 	echo "          1 db entry is the 'ending' of the end of the ltr."
 	echo
-	echo "Output is currently just 2 fasta files containing the pre- and post-ltr chimeric reads."
+	echo "Default output is currently 2 fasta files containing the "
+	echo "	pre- and post-ltr chimeric reads."
+	echo
+	echo "	-q ... yields fastq files instead."
 	echo
 	exit 1
 }
 #	Basically, this is TRUE AND DO ...
 [ $# -eq 0 ] && usage
+
+out='fasta'
+
+while [ $# -ne 0 ] ; do
+	case $1 in
+		-q)
+			shift; out="fastq";;
+		-*)
+			echo ; echo "Unexpected args from: ${*}"; usage ;;
+		*) 
+			break;;
+	esac
+done
+
+
+
+
+
+
+
 
 
 while [ $# -ne 0 ] ; do
@@ -62,7 +85,13 @@ while [ $# -ne 0 ] ; do
 
 	cmd="samtools view $flag -h -F 4 $1"
 	echo $cmd
-	$cmd | awk --posix '
+
+	#	--posix NEEDS to be AFTER any -v settings!
+	$cmd | awk -v base=$base -v out=$out --posix '
+		BEGIN {
+			pre_out=sprintf("%s.pre_ltr.%s",base,out)
+			post_out=sprintf("%s.post_ltr.%s",base,out)
+		}
 		( ( NR % 10000 ) == 0 ){ print "Read "NR" records" }
 
 		( /^@SQ/ ){ ref[substr($2,4)] = substr($3,4) }
@@ -70,14 +99,28 @@ while [ $# -ne 0 ] ; do
 		( ( $6 ~ /^[0-9]{2,}S[0-9IDM]*$/ ) && ( $4 <= 5 ) && ( $3 ~ /beginning/ ) ){
 			split($6,a,"S")
 			clip=a[1]-$4+1
-			print ">"$1"_pre_ltr" >> "'$base.pre_ltr.fasta'"
-			print substr($10,1,clip) >> "'$base.pre_ltr.fasta'"
+			if( out == "fastq" ){
+				print "@"$1"_pre_ltr" >> pre_out
+				print substr($10,1,clip) >> pre_out
+				print "+" >> pre_out
+				print substr($11,1,clip) >> pre_out
+			} else {
+				print ">"$1"_pre_ltr" >> pre_out
+				print substr($10,1,clip) >> pre_out
+			}
 		}
 
 		( ( $6 ~ /^[0-9IDM]*[0-9]{2,}S$/ ) && ( $4 >= ( ref[$3] - (length($10)*0.9) ) ) && ( $3 ~ /ending/ ) ){
 			clip=ref[$3]-$4+2
-			print ">"$1"_post_ltr" >> "'$base.post_ltr.fasta'"
-			print substr($10,clip) >> "'$base.post_ltr.fasta'"
+			if( out == "fastq" ){
+				print "@"$1"_post_ltr" >> post_out
+				print substr($10,clip) >> post_out
+				print "+" >> post_out
+				print substr($11,clip) >> post_out
+			} else {
+				print ">"$1"_post_ltr" >> post_out
+				print substr($10,clip) >> post_out
+			}
 		}'
 
 	#	The 2-digit requirment allows reads as short as 10 to be clipped and pass.
