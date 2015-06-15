3ae0b3bc6ae33504f1b1861dcbcd529924dd385b
Jake <github@jakewendt.com>
2015-06-15 14:09:20 -0700

    Mods to sam_to_bam

--- rins_ccls/sam_to_bam.sh
+++ rins_ccls/sam_to_bam.sh
@@ -1,26 +1,34 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
 while [ $# -ne 0 ] ; do
 	if [ -f "$1" ] ; then
 		echo "Creating bam from (assumed) sam file $1"
 
 		base=${1%.*}		#	drop the extension
-		chmod -w $1
+		ext=${1##*.}		#	just the extension
+#		chmod -w $1
+		mv $1 $1.input
+
+		[[ ${ext,,} =~ sam ]] && flag='-S' || flag=''
 
 		echo "Converting to bam"
 		#	Usage:   samtools view [options] <in.bam>|<in.sam>|<in.cram> [region ...]
-		samtools view -S -b -o $base.unsorted.bam $1
-		chmod -w $base.unsorted.bam
+		samtools view $flag -b -o $base.unsorted.bam $1.input
+#		chmod -w $base.unsorted.bam
+		if [ -s $base.unsorted.bam ] ; then rm $1.input ; fi
 
 		echo "Sorting"
 		#	Usage: samtools sort [options...] [in.bam]
 		#	Legacy usage: samtools sort [options...] <in.bam> <out.prefix>
 		samtools sort -m 2G $base.unsorted.bam $base
-		chmod -w $base.bam
+#		chmod -w $base.bam
+		if [ -s $base.bam ] ; then rm $base.unsorted.bam ; fi
 
 		echo "Indexing"
 		#	Usage: samtools index [-bc] [-m INT] <in.bam> [out.index]
 		samtools index $base.bam
+
+		chmod -w $base.bam
 		chmod -w $base.bam.bai
 	else
 		echo "$1 not a file? Skipping"
