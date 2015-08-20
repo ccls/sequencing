402f48cf1bd32237fc9206a4d6575bf8a7351d2e
Jake <github@jakewendt.com>
2015-08-19 22:22:47 -0700

    Deving AWS scripts.

--- rins_ccls/aws_start_node.sh
+++ rins_ccls/aws_start_node.sh
@@ -1,25 +1,26 @@
 #!/bin/bash
 
-#	aws ec2 describe-images --image-ids ami-e77f81a3
+#	aws ec2 describe-images --image-ids ami-4335cc07
 
-image_id="ami-e77f81a3"
+image_id="ami-4335cc07"
 instance_type="t2.medium"
-volume_size=123
+#volume_size=123
 
 
-command="aws ec2 run-instances --count 1 \
+instance_id=`aws ec2 run-instances --count 1 \
 	--image-id $image_id \
 	--instance-type $instance_type \
 	--key-name devenv-key \
 	--security-groups devenv-sg \
 	--instance-initiated-shutdown-behavior terminate \
-	--query 'Instances[0].InstanceId' \
-	--block-device-mappings '[{\"DeviceName\":\"/dev/xvda\",\"Ebs\":{\"VolumeSize\":$volume_size}}]'"
-echo $command
-#instance_id=`$command`
-#echo $instance_id
+	--query 'Instances[0].InstanceId'`
+#	--block-device-mappings '[{\"DeviceName\":\"/dev/xvda\",\"Ebs\":{\"VolumeSize\":$volume_size}}]'"
+echo $instance_id
 instance_id=`echo $instance_id | sed 's/"//g'`
 echo $instance_id
+
+echo "Waiting a moment to get an ip address ..."
+sleep 5
 
 instance_ip=`aws ec2 describe-instances \
 	--instance-ids $instance_id \
@@ -29,7 +30,8 @@ echo $instance_ip
 
 
 #	start the 1000genomes script in the background.
-#	ssh -n -f -i devenv-key.pem ec2-user@$instance_ip "sh -c 'nohup ~/local/bin/aws_1000genomes.sh /dev/null &'"
+#	if path is set in bashrc (instead of bash_profile), don't need path
+#	ssh -n -f -i devenv-key.pem ec2-user@$instance_ip 'sh -c "( ( nohup aws_1000genomes.sh &>/dev/null ) & )"'
 
 
 #	ssh -n -f user@host "sh -c 'cd /whereever; nohup ./whatever > /dev/null 2>&1 &'"
