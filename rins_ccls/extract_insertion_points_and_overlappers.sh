7445e739e30c7ad5d0c917c8bddee95584af04b6
Jake <github@jakewendt.com>
2016-02-26 09:12:18 -0800

    Renamed  to  for consistency

--- rins_ccls/extract_insertion_points_and_overlappers.sh
+++ rins_ccls/extract_insertion_points_and_overlappers.sh
@@ -1,15 +1,12 @@
 #!/usr/bin/env bash
+set -x
 
-##!/bin/sh -x
 
 index='hg19'
 mapq=0
-
-#	find pattern of base files NOT 
-#		bam replaced with .pre_ltr.bowtie2.hg19.bam
-#			            and .post_ltr.bowtie2.hg19.bam
-base='bowtie2.herv_k113_ltr_ends.__very_sensitive_local.aligned'
+core='bowtie2.herv_k113_ltr_ends.__very_sensitive_local.aligned'
 basedir=`pwd`
+
 
 function usage(){
 	echo
@@ -17,7 +14,7 @@ function usage(){
 	echo
 	echo "`basename $0`"
 	echo
-	echo "`basename $0` [--index STRING] [--mapq INTEGER] [--base 'STRING'] sampledir(s)"
+	echo "`basename $0` [--index STRING] [--mapq INTEGER] [--core 'STRING'] sampledir(s)"
 	echo
 	echo "Example:"
 	echo "  `basename $0` NA211??"
@@ -26,7 +23,7 @@ function usage(){
 	echo "Defaults:"
 	echo "  --index/-i .. : $index"
 	echo "  --mapq/-q  .. : $mapq"
-	echo "  --base/-b ... : $base"
+	echo "  --core/-c ... : $core"
 	echo
 	echo "Output directed to file, similarly named."
 	echo
@@ -39,8 +36,8 @@ while [ $# -ne 0 ] ; do
 			shift; index=$1; shift ;;
 		-q|--mapq*)
 			shift; mapq=$1; shift ;;
-		-b|--b*)
-			shift; base=$1; shift ;;
+		-c|--c*)
+			shift; core=$1; shift ;;
 		-*)
 			echo ; echo "Unexpected args from: ${*}"; usage ;;
 		*)
@@ -61,13 +58,12 @@ logfile=`basename $0`.$index.$quality.$date.out
 	echo "index : $index"
 	echo "mapq : $mapq"
 	echo "quality : $quality"
-	echo "base : $base"
+	echo "core : $core"
 	echo
 
 	#TCGA-41-5651-01A.bowtie2.herv_k113_ltr_ends.__very_sensitive_local.aligned.bam
 
 	while [ $# -ne 0 ] ; do
-#	for bam in `find . -depth 2 -name $findpattern`; do
 		cd $basedir
 
 		#echo $bam
@@ -85,7 +81,7 @@ logfile=`basename $0`.$index.$quality.$date.out
 		#echo $base
 		#	TCGA-02-2483-01A.bowtie2.herv_k113_ltr_ends.__very_sensitive_local.aligned.bam
 
-		echo $base
+		echo $core
 		#	*bowtie2.herv_k113_ltr_ends.__very_sensitive_local.aligned
 
 		#base=${base%.*}		#	drop the extension
@@ -97,23 +93,23 @@ logfile=`basename $0`.$index.$quality.$date.out
 
 		#	TCGA-41-5651-01A.bowtie2.herv_k113_ltr_ends.__very_sensitive_local.aligned.pre_ltr.bowtie2.hg19.bam
 
-		samtools view -q $mapq -F 20 $sample.$base.pre_ltr.bowtie2.$index.bam \
+		samtools view -q $mapq -F 20 $sample.$core.pre_ltr.bowtie2.$index.bam \
 			| awk '{print $3":"$4+length($10)}' \
-			| sort > $sample.$base.pre_ltr.bowtie2.$index.$quality.insertion_points
-		samtools view -q $mapq -F 20 $sample.$base.post_ltr.bowtie2.$index.bam \
+			| sort > $sample.$core.pre_ltr.bowtie2.$index.$quality.insertion_points
+		samtools view -q $mapq -F 20 $sample.$core.post_ltr.bowtie2.$index.bam \
 			| awk '{print $3":"$4}' \
-			| sort > $sample.$base.post_ltr.bowtie2.$index.$quality.insertion_points
-		positions_within_10bp.sh $sample.$base.*.bowtie2.$index.$quality.insertion_points \
-			| sort | uniq -c > $sample.$base.both_ltr.bowtie2.$index.$quality.insertion_points.overlappers
+			| sort > $sample.$core.post_ltr.bowtie2.$index.$quality.insertion_points
+		positions_within_10bp.sh $sample.$core.*.bowtie2.$index.$quality.insertion_points \
+			| sort | uniq -c > $sample.$core.both_ltr.bowtie2.$index.$quality.insertion_points.overlappers
 
-		samtools view -q $mapq -F 4 -f 16 $sample.$base.pre_ltr.bowtie2.$index.bam \
+		samtools view -q $mapq -F 4 -f 16 $sample.$core.pre_ltr.bowtie2.$index.bam \
 			| awk '{print $3":"$4}' \
-			| sort > $sample.$base.pre_ltr.bowtie2.$index.$quality.rc_insertion_points
-		samtools view -q $mapq -F 4 -f 16 $sample.$base.post_ltr.bowtie2.$index.bam \
+			| sort > $sample.$core.pre_ltr.bowtie2.$index.$quality.rc_insertion_points
+		samtools view -q $mapq -F 4 -f 16 $sample.$core.post_ltr.bowtie2.$index.bam \
 			| awk '{print $3":"$4+length($10)}' \
-			| sort > $sample.$base.post_ltr.bowtie2.$index.$quality.rc_insertion_points
-		positions_within_10bp.sh $sample.$base.*.bowtie2.$index.$quality.rc_insertion_points \
-			| sort | uniq -c > $sample.$base.both_ltr.bowtie2.$index.$quality.rc_insertion_points.rc_overlappers
+			| sort > $sample.$core.post_ltr.bowtie2.$index.$quality.rc_insertion_points
+		positions_within_10bp.sh $sample.$core.*.bowtie2.$index.$quality.rc_insertion_points \
+			| sort | uniq -c > $sample.$core.both_ltr.bowtie2.$index.$quality.rc_insertion_points.rc_overlappers
 
 		shift
 	done
